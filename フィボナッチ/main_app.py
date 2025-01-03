5befec68520a8d16b20fba83f3d359ad4495b031
yuga-pn <137192418+yuga-pn@users.noreply.github.com>
2025-01-03 13:46:52 +0900

    Add files via upload

---
+++ フィボナッチ/main_app.py
@@ -0,0 +1,20 @@
+from flask import Flask,request, jsonify
+from application import fib_app
+import math
+
+app = Flask(__name__)
+
+@app.route("/fib",methods=['GET'])
+def fibonacchi():
+    inputs = request.args.get("n","non_input") #!入力されなかった場合の処理
+    if(inputs == "non_input"):
+        return jsonify({"status": 400, "message": "Please enter number!"}), 400
+    status,message,fib_sequence = fib_app.assignment(inputs)
+    if(status == 200):
+        return jsonify({"result": fib_sequence}), status
+    else:
+        return jsonify({"status": status, "message": message}), status
+
+@app.route("/")
+def welcome():
+    return "<p>Welcome to my server! This server calculates fibonacchi sequence. Please enter number you want to know.</p>"
